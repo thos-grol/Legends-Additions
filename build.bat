168eba507644c1226c6fc19e58c9672c4139fb31
A <andrewdo789@gmail.com>
2023-10-22 13:49:09 -0700

    update build tasks

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 del "C:\Files\Games\Battle Brothers\data\mod_legends_dark_age_1_0_0.zip"
-"%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y "C:\Files\Games\Battle Brothers\data\mod_legends_dark_age_1_0_0.zip" "C:\Files\Projects\bbros\mod_legends_additions\Legends-Additions\" -x.git -x.gitattributes -x*.py -x*.bat
+"%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y "C:\Files\Games\Battle Brothers\data\mod_legends_dark_age_1_0_0.zip" "C:\Files\Projects\bbros\Legends-Dark-Age\" -x.git -x.gitattributes -x*.py -x*.bat -.vscode
--- build_brush.bat
+++ build_brush.bat
@@ -1,3 +1,3 @@
-del "C:\Files\Projects\bbros\mod_legends_additions\Legends-Additions\brushes\legends_additions.brush"
-"C:\Files\Projects\bbros\bbrusher.exe" pack "C:\Files\Projects\bbros\mod_legends_additions\Legends-Additions\brushes\legends_additions.brush" "C:\Files\Projects\bbros\brush\legends_additions" --gfxPath "C:\Files\Projects\bbros\mod_legends_additions\Legends-Additions"
+del "C:\Files\Projects\bbros\Legends-Dark-Age\brushes\legends_additions.brush"
+"C:\Files\Projects\bbros\bbrusher.exe" pack "C:\Files\Projects\bbros\Legends-Dark-Age\brushes\legends_additions.brush" "C:\Files\Projects\bbros\brush\legends_additions" --gfxPath "C:\Files\Projects\bbros\Legends-Dark-Age"
 pause
